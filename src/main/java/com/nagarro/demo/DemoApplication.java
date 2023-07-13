8e6837f0e48f80cc1b11e3d9343a017dd2020c61
Priyanshu Garg <priyanshu.garg02@nagarro.com>
2023-07-13 18:32:04 +0530

    first commit

---
+++ src/main/java/com/nagarro/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.nagarro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats!! Application is deployed successfully on Azure";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
